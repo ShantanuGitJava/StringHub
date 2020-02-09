579bd8b35bc7fd55f5cfec84cc5124d7afe8c0b3
shantanu <shantanukumarlenka94@gmail.com>
2020-02-09 16:50:51 +0530

    Add AnagramStringCheckExample Project

---
+++ AnagramStringCheckExample/src/main/java/org/java/algo/AnagramStringCheckApp.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package org.java.algo;
+
+/**
+ * @author shantanu
+ * @date 07-May-2019 9:58:03 am
+ */
+public class AnagramStringCheckApp {
+    public static boolean checkAnagram(String word, String anagram) {
+	if (word.length() != anagram.length()) {
+	    return false;
+	}
+	char[] characters = word.toLowerCase().toCharArray();
+	for (char ch : characters) {
+	    int index = anagram.toLowerCase().indexOf(ch);
+	    if (index != -1) {
+		anagram = anagram.substring(0, index) + anagram.substring(index + 1);
+	    } else {
+		return false;
+	    }
+	}
+	return true;
+    }
+
+}
---
+++ AnagramStringCheckExample/src/main/java/org/java/test/AnagramStringCheckAppTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.java.test;
+
+import org.java.algo.AnagramStringCheckApp;
+
+/**
+ * @author shantanu
+ * @date 07-May-2019 10:10:54 am
+ */
+public class AnagramStringCheckAppTest {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+	System.out.println(AnagramStringCheckApp.checkAnagram("java", "avja"));
+    }
+
+}
